bd83d7a84e37b2bfcd5652c58236f062a558a9c7
Your Name <you@example.com>
2024-09-04 14:50:15 -0400

    Implement reducers for cart slice

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -82,4 +82,44 @@ function ProductList() {
                             <h1 className='cart'>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                                     <rect width="156" height="156" fill="none"></rect>
-                                    <circle 
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
+                                </svg>
+                            </h1>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            {!showCart ? (
+                <div className="product-grid">
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h2>{category.category}</h2>
+                            <div className="product-grid">
+                                {category.plants.map((plant, idx) => (
+                                    <div className="product-card" key={idx}>
+                                        <img src={plant.image} alt={plant.name} className="product-image" />
+                                        <h3 className="product-title">{plant.name}</h3>
+                                        <p className="product-description">{plant.description}</p>
+                                        <p className="product-cost">{plant.cost}</p>
+                                        <button 
+                                            className="product-button" 
+                                            onClick={() => handleAddToCart(plant)}
+                                        >
+                                            Add to Cart
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <CartItem onContinueShopping={handleContinueShopping} />
+            )}
+        </div>
+    );
+}
+
+export default ProductList;
